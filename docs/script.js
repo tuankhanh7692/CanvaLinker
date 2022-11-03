cf3454eea2b9bdae19b55cace5f1dad207e129f7
tuankhanh7692 <73464690+tuankhanh7692@users.noreply.github.com>
2022-11-04 03:16:50 +0700

    Create script.js

---
+++ docs/script.js
@@ -0,0 +1,1 @@
+window.location.href = "https://www.canva.com/design/DAFQ0Kqw2I4/dVvUXTaKcM2ZLYqmCm7tSQ/view?utm_content=DAFQ0Kqw2I4&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink";
